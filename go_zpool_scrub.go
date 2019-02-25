df7df8203dc05063e6913d6750b7bdafb69eb723
Grant Novota <grantnovota@biot21-58-dhcp.int.colorado.edu>
2019-02-25 16:28:51 -0700

    initial commit

---
+++ go_zpool_scrub.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+   //"fmt"
+   //"os"
+   "os/exec"
+   //"bytes"
+   //"io"
+)
+
+func main() {
+  cmd := exec.Command("ls")
+
+  stdout, err := cmd.Output()
+
+  if err != nil {
+    println(err.Error())
+    return
+  }
+
+  print(string(stdout))
+}
+
+
+
+// func main() {
+//     app := "echo"
+//     //app := "buah"
+
+//     arg0 := "-e"
+//     arg1 := "Hello world"
+//     arg2 := "\n\tfrom"
+//     arg3 := "golang"
+
+//     cmd := exec.Command(app, arg0, arg1, arg2, arg3)
+//     stdout, err := cmd.Output()
+
+//     if err != nil {
+//         println(err.Error())
+//         return
+//     }
+
+//     print(string(stdout))
+// }
