5456543230cc563cd1dd76ae908ee9d87bcb90f4
Grant Novota <grantnovota@biot21-58-dhcp.int.colorado.edu>
2019-03-01 10:12:39 -0700

    added pool struct

--- go_zpool_scrub.go
+++ go_zpool_scrub.go
@@ -33,12 +33,13 @@ func main() {
 
   ln := strings.Split(string(stdout), "\n") //split into lines
   //create array for storing pool structs that has length ln - 1
-  var pools [len(ln) - 1]Pool
+  pool_size := len(n) - 1
+  var pools [pool_size]Pool
   for i := 1; i < len(ln); i++ { //iterate thru each line
     //fmt.Println(ln[i])
     s := strings.Split(ln[i], " ") //split each line
     pool := s[0] //name of the pool ->start of each line
-    //fmt.Println(pool)
+    fmt.Println(pool)
     pools[i-1].Name = pool
   }
   
