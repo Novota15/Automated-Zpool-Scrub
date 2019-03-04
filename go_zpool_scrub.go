2f24346998573a915c88f76f526f945a22ba4d4b
Grant Novota <grantnovota@biot21-58-dhcp.int.colorado.edu>
2019-03-04 10:19:43 -0700

    added pool struct

--- go_zpool_scrub.go
+++ go_zpool_scrub.go
@@ -3,7 +3,7 @@ package main
 import (
    "fmt"
    "strings"
-   //"os"
+   "os"
    "os/exec"
    //"bytes"
    //"io"
@@ -17,27 +17,27 @@ type Pool struct {
 //TO DO: modularize code into separate functions
 
 func Get_zpool_Names() []Pool {
-  // cmd := exec.Command("bash", "-c", "zpool list")
+  cmd := exec.Command("bash", "-c", "zpool list")
 
-  // stdout, err := cmd.Output()
+  stdout, err := cmd.Output()
 
-  // if err != nil {
-  //   println(err.Error())
-  //   return
-  // }
+  if err != nil {
+    println(err.Error())
+    return
+  }
 
-  // ln := strings.Split(string(stdout), "\n") //split into lines
-  // //create array for storing pool structs that has length ln - 2
-  // pool_size := len(ln) - 2
-  // pools := make([]Pool, pool_size)
-  // for i := 1; i < len(ln) - 1; i++ { //iterate thru each line
-  //   //fmt.Println(ln[i])
-  //   s := strings.Split(ln[i], " ") //split each line
-  //   pool := s[0] //name of the pool ->start of each line
-  //   fmt.Println(pool)
-  //   pools[i-1].Name = pool
-  // }
-  pools := make([]Pool, 5)
+  ln := strings.Split(string(stdout), "\n") //split into lines
+  //create array for storing pool structs that has length ln - 2
+  pool_size := len(ln) - 2
+  pools := make([]Pool, pool_size)
+  for i := 1; i < len(ln) - 1; i++ { //iterate thru each line
+    //fmt.Println(ln[i])
+    s := strings.Split(ln[i], " ") //split each line
+    pool := s[0] //name of the pool ->start of each line
+    fmt.Println(pool)
+    pools[i-1].Name = pool
+  }
+  //pools := make([]Pool, 5)
   return pools
 }
 
