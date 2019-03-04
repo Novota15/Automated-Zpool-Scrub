fa1f7d11a5c3195e1c613e3bc4333882171d6277
Grant Novota <grantnovota@biot21-58-dhcp.int.colorado.edu>
2019-03-04 16:56:51 -0700

    started sorting algorithm

--- go_zpool_scrub.go
+++ go_zpool_scrub.go
@@ -144,24 +144,3 @@ func main() {
 
 }
 
-
-
-// func main() {
-//     app := "echo"
-//     //app := "buah"
-
-//     arg0 := "-e"
-//     arg1 := "Hello world"
-//     arg2 := "\n\tfrom"
-//     arg3 := "golang"
-
-//     cmd := exec.Command(app, arg0, arg1, arg2, arg3)
-//     stdout, err := cmd.Output()
-
-//     if err != nil {
-//         println(err.Error())
-//         return
-//     }
-
-//     print(string(stdout))
-// }
