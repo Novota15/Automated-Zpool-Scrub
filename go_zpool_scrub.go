794e248e2b1fa5f75161e8581b1a35a75aa11181
Grant Novota <grantnovota@biot21-58-dhcp.int.colorado.edu>
2019-03-06 08:59:22 -0700

    started sorting algorithm

--- go_zpool_scrub.go
+++ go_zpool_scrub.go
@@ -88,7 +88,8 @@ func Get_zpool_Scrub_Date(pools []Pool) {
         year := string(pool.Scan[i-4:i])
         fmt.Println(month, day, year)
         date := year + "-" + month + "-" + day
-        pool.Scan_Date, _ = time.Parse(shortForm, date)
+        t, _ = time.Parse(shortForm, date)
+        pool.Scan_Date = t
         //fmt.Println(t)
         //pool.Scan_Date = Convert_Date_to_Int(month, day, year)
         fmt.Println(pool.Scan_Date)
