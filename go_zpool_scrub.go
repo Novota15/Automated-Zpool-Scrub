cd803e987b7308b4e69e88fdc8d844059230e023
Grant Novota <grantnovota@biot21-58-dhcp.int.colorado.edu>
2019-03-06 09:28:03 -0700

    started sorting algorithm

--- go_zpool_scrub.go
+++ go_zpool_scrub.go
@@ -98,35 +98,41 @@ func Get_zpool_Scrub_Date(pools []Pool) {
   }
 }
 
-func swap(a Pool, b Pool) {
-  temp = *a
-  *a = *b
-  *b = temp
-}
+// func swap(a Pool, b Pool) {
+//   temp = *a
+//   *a = *b
+//   *b = temp
+// }
 
-func partition(pools []Pool, low int, high int) int{
-  pivot := pools[high]
-  i := low - 1
+// func partition(pools []Pool, low int, high int) int{
+//   pivot := pools[high]
+//   i := low - 1
 
-  for j := low; j <= high - 1; j++ {
-    if time.Date(pools[j].Scan_Date).After(time.Date(pivot.Scan_Date)) {
-      i++
-      swap(pools[i], pools[j])
-    }
-  }
-  swap(pools[i+1], pools[high])
-  return (i+1)
-}
+//   for j := low; j <= high - 1; j++ {
+//     if time.Date(pools[j].Scan_Date).After(time.Date(pivot.Scan_Date)) {
+//       i++
+//       swap(pools[i], pools[j])
+//     }
+//   }
+//   swap(pools[i+1], pools[high])
+//   return (i+1)
+// }
 
-func Sort_by_Date(pools []Pool, low int, high int) { //quicksort algorithm
-  t1 := time.Date(pools[low].Scan_Date)
-  t2 := time.Date(pools[high].Scan_Date)
-  if t1.After(t2) {
-    pi := partition(pools, low, high)
-    Sort_by_Date(pools, low, pi - 1)
-    Sort_by_Date(pools, pi + 1, high)
-  }
-}
+// func Sort_by_Date(pools []Pool, low int, high int) { //quicksort algorithm
+//   t1 := time.Date(pools[low].Scan_Date)
+//   t2 := time.Date(pools[high].Scan_Date)
+//   if t1.After(t2) {
+//     pi := partition(pools, low, high)
+//     Sort_by_Date(pools, low, pi - 1)
+//     Sort_by_Date(pools, pi + 1, high)
+//   }
+// }
+
+// func Sort_by_Date(pools []Pool) {
+//   for _, pool := range pools {
+
+//   }
+// }
 
 func main() {
   //arg0 := "zpool status"
@@ -138,7 +144,7 @@ func main() {
   pools := Get_zpool_Names()
   Get_zpool_scan(pools)
   Get_zpool_Scrub_Date(pools)
-  Sort_by_Date(pools, 0, 1)
+  //Sort_by_Date(pools)
   //now := time.Now()
   //fmt.Println(now)
   //call zpool status on each pool and store status in pool struct
