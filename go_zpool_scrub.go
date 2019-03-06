279f4fd5e70dc60780926d9feaa24df7b76d127e
Grant Novota <grantnovota@biot21-58-dhcp.int.colorado.edu>
2019-03-06 10:06:39 -0700

    started sorting algorithm

--- go_zpool_scrub.go
+++ go_zpool_scrub.go
@@ -138,14 +138,14 @@ func Get_zpool_Scrub_Date(pools []Pool) {
 func Find_Oldest_Scrub(pools []Pool) int{
   j := 0
   fmt.Println(pools[1].Scan_Date)
-  // for i := 1; i < len(pools); i++ {
-  //   t1 := time.Date(pools[j].Scan_Date)
-  //   t2 := time.Date(pools[i].Scan_Date)
-  //   if t1.After(t2) {
-  //     j = i
-  //   }
-  // }
-  // fmt.Println(pools[j].Name)
+  for i := 1; i < len(pools); i++ {
+    t1 := time.Date(pools[j].Scan_Date)
+    t2 := time.Date(pools[i].Scan_Date)
+    if t1.After(t2) {
+      j = i
+    }
+  }
+  fmt.Println(pools[j].Name)
   return j
 }
 
@@ -160,7 +160,8 @@ func main() {
   Get_zpool_scan(pools)
   Get_zpool_Scrub_Date(pools)
   j := Find_Oldest_Scrub(pools)
-  fmt.Println(j)
+  fmt.Println("Oldest Scrub:")
+  fmt.Println(pools[j])
   //now := time.Now()
   //fmt.Println(now)
   //call zpool status on each pool and store status in pool struct
