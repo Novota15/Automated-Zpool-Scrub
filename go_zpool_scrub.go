439f444d63179130d03189b3d5540c6062bc76ef
Grant Novota <grantnovota@biot21-58-dhcp.int.colorado.edu>
2019-03-08 09:16:27 -0700

    acccounting for never scrubbed pool

--- go_zpool_scrub.go
+++ go_zpool_scrub.go
@@ -115,6 +115,8 @@ func Find_Oldest_Scrub(pools []Pool) int{
   j = 0
   for i := 1; i < len(pools); i++ { //if all have been scrubbed, find the oldest
     if pools[i].Scanned == true {
+      fmt.Println(pools[j].Scan_Date)
+      fmt.Println(pools[i].Scan_Date)
       if pools[j].Scan_Date.Before(pools[i].Scan_Date) {
       j = i
       }
