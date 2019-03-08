52c01eec5c967c877297701cec3779ea02ffa466
Grant Novota <grantnovota@biot21-58-dhcp.int.colorado.edu>
2019-03-08 09:19:23 -0700

    acccounting for never scrubbed pool

--- go_zpool_scrub.go
+++ go_zpool_scrub.go
@@ -88,7 +88,7 @@ func Get_zpool_Scrub_Date(pools []Pool) {
         }
         i = i + 13
         year := string(pools[k].Scan[i-4:i])
-        //fmt.Println(month, day, year)
+        fmt.Println(month, day, year)
         date := year + "-" + month + "-" + day
         t, _ := time.Parse(shortForm, date)
         pools[k].Scan_Date = t
